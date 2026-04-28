2d482a092b8ca75971cf5c448ae219245761fe9b
Shraddha Akolkar <shraddhaaakolkar@gmail.com>
2026-04-28 12:40:57 +0530

    banner done

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import Banner from "../components/Banner/Banner"
+import Navbar from "../Pages/Header/Navbar"
+import Cards from "../components/Cards/Cards"
+import Footer from "../Pages/Footer/Footer"
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+      <Banner/>
+      <Cards/>
+     <Footer/>
+
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="doctors-header">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-10 col-lg-8 text-center">
+          <h1 className="main-title">
+            Meet Our Most Experienced Doctors
+          </h1>
+          <p className="sub-title">
+            Our Experienced Medical Team: Your Partners in Health
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
